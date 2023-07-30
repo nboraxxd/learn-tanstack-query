c859f771d8a89bc8444fcc946b69999fd4ab6a5b
bora <nhatbaoxxd@gmail.com>
2023-07-30 13:31:44 +0700

    Caching in tantack query

--- frontend/src/pages/Students/Students.tsx
+++ frontend/src/pages/Students/Students.tsx
@@ -9,51 +9,58 @@ export default function Students() {
   const queryString: { page?: string } = useQueryString()
   const page = Number(queryString.page || 1)
 
-  const { data, isLoading } = useQuery({ queryKey: ['students', page], queryFn: () => getStudents(page, LIMIT) })
+  const { data, isLoading, isFetching } = useQuery({
+    queryKey: ['students', page],
+    queryFn: () => getStudents(page, LIMIT),
+    // staleTime: 10 * 1000,
+    // cacheTime: 5 * 1000,
+    keepPreviousData: true,
+  })
   const totalStudentCount = Number(data?.headers['x-total-count'] || 0)
   const totalPage = Math.ceil(totalStudentCount / LIMIT)
 
+  console.log('🔥 ~ isLoading ~ isFetching ~ data:', isLoading, isFetching, data?.data)
   return (
     <div>
-      <h1 className='text-lg'>Students</h1>
+      <h1 className="text-lg">Students</h1>
       {isLoading === true && (
-        <div role='status' className='mt-6 animate-pulse'>
-          <div className='mb-4 h-4 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <div className='h-10 rounded bg-gray-200 dark:bg-gray-700' />
-          <span className='sr-only'>Loading...</span>
+        <div role="status" className="mt-6 animate-pulse">
+          <div className="mb-4 h-4 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="mb-2.5 h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <div className="h-10 rounded bg-gray-200 dark:bg-gray-700" />
+          <span className="sr-only">Loading...</span>
         </div>
       )}
       {isLoading === false && (
         <>
-          <div className='relative mt-6 overflow-x-auto shadow-md sm:rounded-lg'>
-            <table className='w-full text-left text-sm text-gray-500 dark:text-gray-400'>
-              <thead className='bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400'>
+          <div className="relative mt-6 overflow-x-auto shadow-md sm:rounded-lg">
+            <table className="w-full text-left text-sm text-gray-500 dark:text-gray-400">
+              <thead className="bg-gray-50 text-xs uppercase text-gray-700 dark:bg-gray-700 dark:text-gray-400">
                 <tr>
-                  <th scope='col' className='py-3 px-6'>
+                  <th scope="col" className="py-3 px-6">
                     #
                   </th>
-                  <th scope='col' className='py-3 px-6'>
+                  <th scope="col" className="py-3 px-6">
                     Avatar
                   </th>
-                  <th scope='col' className='py-3 px-6'>
+                  <th scope="col" className="py-3 px-6">
                     Name
                   </th>
-                  <th scope='col' className='py-3 px-6'>
+                  <th scope="col" className="py-3 px-6">
                     Email
                   </th>
-                  <th scope='col' className='py-3 px-6'>
-                    <span className='sr-only'>Action</span>
+                  <th scope="col" className="py-3 px-6">
+                    <span className="sr-only">Action</span>
                   </th>
                 </tr>
               </thead>
@@ -61,24 +68,24 @@ export default function Students() {
                 {data?.data.map((student, index) => (
                   <tr
                     key={student.id}
-                    className='border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600'
+                    className="border-b bg-white hover:bg-gray-50 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-600"
                   >
-                    <td className='py-4 px-6'>{page * 10 - 10 + (index + 1)}</td>
-                    <td className='py-4 px-6'>
-                      <img src={student.avatar} alt={student.last_name} className='h-5 w-5' />
+                    <td className="py-4 px-6">{student.id - 5}</td>
+                    <td className="py-4 px-6">
+                      <img src={student.avatar} alt={student.last_name} className="h-5 w-5" />
                     </td>
-                    <th scope='row' className='whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white'>
+                    <th scope="row" className="whitespace-nowrap py-4 px-6 font-medium text-gray-900 dark:text-white">
                       {student.last_name}
                     </th>
-                    <td className='py-4 px-6'>{student.email}</td>
-                    <td className='py-4 px-6 text-right'>
+                    <td className="py-4 px-6">{student.email}</td>
+                    <td className="py-4 px-6 text-right">
                       <Link
-                        to='/students/1'
-                        className='mr-5 font-medium text-blue-600 hover:underline dark:text-blue-500'
+                        to="/students/1"
+                        className="mr-5 font-medium text-blue-600 hover:underline dark:text-blue-500"
                       >
                         Edit
                       </Link>
-                      <button className='font-medium text-red-600 dark:text-red-500'>Delete</button>
+                      <button className="font-medium text-red-600 dark:text-red-500">Delete</button>
                     </td>
                   </tr>
                 ))}
@@ -86,18 +93,18 @@ export default function Students() {
             </table>
           </div>
 
-          <div className='mt-6 flex justify-center'>
-            <nav aria-label='Page navigation example'>
-              <ul className='inline-flex -space-x-px'>
+          <div className="mt-6 flex justify-center">
+            <nav aria-label="Page navigation example">
+              <ul className="inline-flex -space-x-px">
                 <li>
                   {page <= 1 ? (
-                    <span className='cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-300'>
+                    <span className="cursor-not-allowed rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-300">
                       Previous
                     </span>
                   ) : (
                     <Link
                       to={`/students?page=${page - 1}`}
-                      className='cursor-pointer rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                      className="cursor-pointer rounded-l-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                     >
                       Previous
                     </Link>
@@ -113,7 +120,7 @@ export default function Students() {
                         <Link
                           className={classNames('border border-gray-300 py-2 px-3 leading-tight', {
                             'bg-gray-100 font-semibold text-blue-500': isActive,
-                            'hover:bg-gray-100 hover:text-gray-700': !isActive
+                            'hover:bg-gray-100 hover:text-gray-700': !isActive,
                           })}
                           to={`/students?page=${pageNumber}`}
                         >
@@ -124,13 +131,13 @@ export default function Students() {
                   })}
                 <li>
                   {page >= totalPage ? (
-                    <span className='cursor-not-allowed rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-300'>
+                    <span className="cursor-not-allowed rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-300">
                       Next
                     </span>
                   ) : (
                     <Link
                       to={`/students?page=${page + 1}`}
-                      className='cursor-pointer rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-700 hover:bg-gray-100 hover:text-gray-900'
+                      className="cursor-pointer rounded-r-lg border border-gray-300 bg-white py-2 px-3 leading-tight text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                     >
                       Next
                     </Link>
